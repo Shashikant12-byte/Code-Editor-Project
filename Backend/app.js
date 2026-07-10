88d9a0f30a813f1117349b6eac5a45d43995aba8
Shashi Kant Yadav <sima69246@gmail.com>
2026-07-10 22:16:48 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ connectDB();
 
 const app=express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://charis-code-frontend.onrender.com",
     credentials: true,
     methods: ["GET", "POST"]
 }));
