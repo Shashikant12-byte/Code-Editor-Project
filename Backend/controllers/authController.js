93370c3569b3417c696b66c7af70ba8fc4affeae
Shashi Kant Yadav <sima69246@gmail.com>
2026-07-30 21:54:23 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -64,7 +64,7 @@ const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: process.env.NODE_ENVIOURNMENT === "production",
             sameSite: 'none',
             maxAge: 1 * 24 * 60 * 60 * 1000
 
